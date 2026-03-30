3f6d5159e1bbe57adeca1dcb473ff2adeb1ea8c3
informati <46190977+Matthieusz@users.noreply.github.com>
2026-03-30 17:02:35 +0200

    feat: add dark mode toggle

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
     import "../app.css";
     import Header from "../components/Header.svelte";
-
+    import { ModeWatcher } from "mode-watcher";
     const { children } = $props();
 </script>
 
 <div class="grid h-svh grid-rows-[auto_1fr]">
     <Header />
     <main class="overflow-y-auto">
+        <ModeWatcher />
         {@render children()}
     </main>
 </div>
